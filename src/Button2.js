213679f71931daef0237bac405b91b7e9a98164d
Alexander <nemechekalexander@gmail.com>
2019-04-06 23:16:38 +0300

    new live channels

--- src/Button2.js
+++ src/Button2.js
@@ -3,7 +3,7 @@ import {withButton} from './ButtonProvider'
 
 // https://newsapi.org/v2/everything?domains=wsj.com&apiKey=f64c9be83f094f43a2c3954a6c1ec8aa`
 const Button2 = (props) => {
-    
+   
     return (
             <div className = "transitions">
             <button className = " button1" onClick = {props.handleToggle}>Watch</button>
@@ -16,8 +16,10 @@ const Button2 = (props) => {
                         <p className = 'live' onClick = {()=> props.showVideo('https://www.youtube.com/watch?v=hNJ5v-5vHk0', 'NBC-2 LIVE')}>NBC-2 News LIVE </p>
                         <p className = 'live' onClick = {()=> props.showVideo('https://www.youtube.com/watch?v=IukZ7gOGPj8', 'ABC-7 SWFL LIVE')}>ABC-7 News LIVE</p>
                         <p className = 'live' onClick = {()=> props.showVideo('https://www.youtube.com/watch?v=NvqKZHpKs-g', 'DW News LIVE')}>DW News LIVE</p>
-                        
-                        </div>
+                        <p className = 'live' onClick = {()=> props.showVideo('https://www.youtube.com/watch?v=9iQ17RA-6r0', 'FOX News LIVE')}>FOX News LIVE </p>
+                        <p className = 'live' onClick = {()=> props.showVideo('https://www.youtube.com/watch?v=IFAcqaNzNSc', 'Russia Today LIVE')}>RT News LIVE</p>
+                        <p className = 'live' onClick = {()=> props.showVideo( 'https://www.youtube.com/watch?v=rQSwh3bgs5k' , 'ABC News Australia')}>ABC Australia LIVE</p>
+                    </div>
                 </div>
 
             :
