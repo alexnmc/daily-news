c1d7a5820862a67cc114c06d56dc4c5c6b4d0df2
Alexander <nemechekalexander@gmail.com>
2019-04-07 13:59:08 +0300

    keyframe fade

--- src/Button2.js
+++ src/Button2.js
@@ -13,12 +13,12 @@ const Button2 = (props) => {
                     <div className = "navbar">
                         <p className = 'live' onClick = {()=> props.showVideo('https://www.youtube.com/watch?v=lrX6ktLg8WQ', 'SKY NEWS LIVE')}>Sky News LIVE</p>
                         <p className = 'live' onClick = {()=> props.showVideo('https://www.youtube.com/watch?v=5avbHxRppxo', 'EURONEWS LIVE')}>Euronews LIVE</p>
-                        <p className = 'live' onClick = {()=> props.showVideo('https://www.youtube.com/watch?v=hNJ5v-5vHk0', 'NBC-2 LIVE')}>NBC-2 News LIVE </p>
-                        <p className = 'live' onClick = {()=> props.showVideo('https://www.youtube.com/watch?v=IukZ7gOGPj8', 'ABC-7 SWFL LIVE')}>ABC-7 News LIVE</p>
                         <p className = 'live' onClick = {()=> props.showVideo('https://www.youtube.com/watch?v=NvqKZHpKs-g', 'DW News LIVE')}>DW News LIVE</p>
                         <p className = 'live' onClick = {()=> props.showVideo('https://www.youtube.com/watch?v=9iQ17RA-6r0', 'FOX News LIVE')}>FOX News LIVE </p>
                         <p className = 'live' onClick = {()=> props.showVideo('https://www.youtube.com/watch?v=IFAcqaNzNSc', 'RT News LIVE')}>RT News LIVE</p>
                         <p className = 'live' onClick = {()=> props.showVideo('https://www.youtube.com/watch?v=rQSwh3bgs5k', 'ABC News Australia')}>ABC Australia LIVE</p>
+                        <p className = 'live' onClick = {()=> props.showVideo('https://www.youtube.com/watch?v=hNJ5v-5vHk0', 'NBC-2 LIVE')}>NBC-2 News LIVE </p>
+                        <p className = 'live' onClick = {()=> props.showVideo('https://www.youtube.com/watch?v=IukZ7gOGPj8', 'ABC-7 SWFL LIVE')}>ABC-7 News LIVE</p>
                     </div>
                 </div>
 
