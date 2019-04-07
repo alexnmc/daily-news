4c301fa86a2c2feacfe5099bc188662d1af62981
Alexander <nemechekalexander@gmail.com>
2019-04-07 17:20:17 +0300

    new keyframe added

--- src/Video.js
+++ src/Video.js
@@ -7,7 +7,7 @@ const Video = (props) => {
     return(
  <div className = 'video' >
     <div className = 'videoWrap'>
-        <ReactPlayer className = 'reactPlayer' url={props.videoUrl} height='100%' width='100%' playing />
+        <ReactPlayer className = 'reactPlayer' url={props.videoUrl} height='100%' width='100%' playing  style = {props.style}/>
     </div>
  </div>  
     )
