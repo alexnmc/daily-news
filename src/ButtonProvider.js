26ddc78a8a98aef8a3b65a482f5ed38096b0367a
Alexander <nemechekalexander@gmail.com>
2019-07-19 21:18:07 +0300

    new

--- src/ButtonProvider.js
+++ src/ButtonProvider.js
@@ -156,7 +156,6 @@ class ButtonProvider extends Component {
                         toggle: !prevState.toggle, 
                         toggle2: false,
                         style:{ animation:''},
-                       
                     }
         })
        
